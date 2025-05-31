a01a647fbfd14f7b2c117b4acbdd4f3f4f3bdd64
Nurseiit Sharip <nurseit.js@gmail.com>
2025-05-31 14:41:11 +0500

    Refactor Home page layout to include Container component and add Review section; update Button component styles for improved usability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import { Security } from "@features/security";
 import { LiveMarket } from "@features/livemarket";
 import { Regulation } from "@features/regulation";
+import { Container } from "@layout/container";
+import { Review } from "@features/review";
 
 export default function Home() {
   return (
-    <div className="container px-4 mx-auto py-10 flex flex-col gap-10">
-      <LiveMarket />
-      <Security />
-      <Regulation />
+    <div className="py-10 flex flex-col gap-10">
+      <Container className="flex flex-col gap-10 px-4">
+        <LiveMarket />
+        <Security />
+        <Regulation />
+      </Container>
+      <Review />
     </div>
   );
 }
