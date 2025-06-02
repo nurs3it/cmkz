5cbdc563bec78e0f2745c7f5f0380376d42fda34
Nurseiit Sharip <nurseit.js@gmail.com>
2025-06-03 02:54:40 +0500

    Refactor Contacts page to utilize nested translations for metadata and UI elements, enhancing internationalization support and improving localization consistency.

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,28 +1,32 @@
 import { Container } from "@layout/container";
 import Link from "next/link";
 import { menu } from "@/components/features/header/data";
-import { useTranslations } from "next-intl";
 import { Metadata } from "next";
 import { getMessages } from "next-intl/server";
+import { getNestedTranslation } from "@/utils/translations";
 
 export async function generateMetadata(): Promise<Metadata> {
   const messages = await getMessages();
   return {
-    title: messages.page_titles.contacts,
+    title: getNestedTranslation(messages, "page_titles.contacts"),
   };
 }
 
-export default function Contacts() {
-  const t = useTranslations();
+export default async function Contacts() {
+  const messages = await getMessages();
   const contacts = menu.find((item) => item.code === "contacts");
 
   return (
     <Container className="flex flex-col gap-4 py-10 px-4">
-      <h1 className="text-2xl font-bold">{t("page_titles.contacts")}</h1>
+      <h1 className="text-2xl font-bold">
+        {getNestedTranslation(messages, "page_titles.contacts")}
+      </h1>
       <ul>
         {contacts?.children?.map((item) => (
           <li className="text-lg underline text-primary" key={item.code}>
-            <Link href={item.href || ""}>{t(item.labelKey)}</Link>
+            <Link href={item.href || ""}>
+              {getNestedTranslation(messages, item.labelKey)}
+            </Link>
           </li>
         ))}
       </ul>
